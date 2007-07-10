90f5c760da43039044fc2451e8127cff10a55289
jnmbk <jnmbk@23da626b-2e77-438c-8063-04e1a2c34933>
2007-07-10 13:33:54 +0000

    some more progress with tray icon

--- pysozlukqt/pysozlukglobals.py
+++ pysozlukqt/pysozlukglobals.py
@@ -21,8 +21,17 @@ mainWindowFileName = os.path.join(dataDirectory, "mainWindow.ui")
 
 #absolute paths
 database = os.path.join(dataDirectory, databaseFileName)
-icon = "/usr/share/icons/hicolor/scalable/apps/pysozluk-qt.svg"
-
+#icons
+#TODO: We should fix this mess, there are two ways to get rid of it
+# 1. (very dirty) Assume that person is using KDE, and use KDE 3.5 api to find
+#    the current icon directory
+# 2. Include Tulliana-2.0 icon set with pysozluk-qt
+#TODO: We sholud compile the svg icon to pngs, otherwise pisi package lacks icon
+iconPath = "/usr/share/icons/Tulliana-2.0/16x16"
+icon_main = "/usr/share/icons/hicolor/scalable/apps/pysozluk-qt.svg"
+icon_translate = os.path.join(iconPath, "apps/locale.png")
+icon_configure = os.path.join(iconPath, "actions/configure.png")
+icon_exit = os.path.join(iconPath, "actions/exit.png")
 #other
 version = "0.1"
 
--- pysozlukqt/trayicon.py
+++ pysozlukqt/trayicon.py
@@ -11,10 +11,33 @@
 # Please read the COPYING file.
 
 from PyQt4 import QtGui
+from uic.Compiler.qtproxies import QtCore
+import gettext
 import pysozlukglobals
 
+_ = gettext.translation('pysozluk-qt', fallback=True).ugettext
+
 class PySozlukTrayIcon(QtGui.QSystemTrayIcon):
-    def __init__(self, ui):
-        QtGui.QSystemTrayIcon.__init__(self, QtGui.QIcon(pysozlukglobals.icon))
-        self.ui = ui
-        
+    def __init__(self, ui, app):
+        QtGui.QSystemTrayIcon.__init__(
+            self, QtGui.QIcon(pysozlukglobals.icon_main))
+        action_translate = QtGui.QAction(
+            QtGui.QIcon(pysozlukglobals.icon_translate), _("Translate"), self)
+        action_configure = QtGui.QAction(
+            QtGui.QIcon(pysozlukglobals.icon_configure),
+            _("Configure PySozluk-Qt"), self)
+        action_exit = QtGui.QAction(
+            QtGui.QIcon(pysozlukglobals.icon_exit),
+            _("Exit"), self)
+
+        self.menu = QtGui.QMenu()
+        self.menu.addAction(action_translate)
+        self.menu.addSeparator()
+        self.menu.addAction(action_configure)
+        self.menu.addSeparator()
+        self.menu.addAction(action_exit)
+
+        #QtCore.QObject.connect(
+        #    action_exit, QtCore.SIGNAL("triggered()"), app.exit)
+
+        self.setContextMenu(self.menu)
