c38455dfce1daf7920b042970f4634a9c94da9e6
jnmbk <jnmbk@23da626b-2e77-438c-8063-04e1a2c34933>
2007-08-06 10:35:46 +0000

    gettext is perfect but I may still switch to .ts :P

--- trunk/pysozlukqt/__init__.py
+++ trunk/pysozlukqt/__init__.py
@@ -24,19 +24,6 @@ import trayicon
 locale.setlocale(locale.LC_ALL, "")
 _ = translation('pysozluk-qt', fallback=True).ugettext
 
-def retranslateUi(ui):
-    ui.label.setText(_("Keyword:"))
-    ui.pushButton.setText(_("&Search"))
-    ui.menuFile.setTitle(_("&File"))
-    ui.menuHelp.setTitle(_("&Help"))
-    ui.actionAbout_Pysozluk_Qt.setText(_("About &Pysozluk-Qt"))
-    ui.actionAbout_Qt.setText(_("About &Qt"))
-    ui.actionConfigure.setText(_("&Configure PySozluk-Qt"))
-    ui.actionOffline.setText(_("&Offline"))
-    ui.actionSave.setText(_("&Save"))
-    ui.actionQuit.setText(_("&Quit"))
-    ui.setWindowTitle(_("Pysozluk-Qt"))
-
 def makeConnections(ui, core, app):
     QtCore.QObject.connect(
         ui.actionAbout_Pysozluk_Qt,QtCore.SIGNAL("activated()"),core.about)
@@ -53,6 +40,19 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         QtGui.QMainWindow.__init__(self)
         self.setupUi(self)
 
+    def retranslateUi(self, MainWindow):
+        self.label.setText(_("Keyword:"))
+        self.pushButton.setText(_("&Search"))
+        self.menuFile.setTitle(_("&File"))
+        self.menuHelp.setTitle(_("&Help"))
+        self.actionAbout_Pysozluk_Qt.setText(_("About &PySozluk-Qt"))
+        self.actionAbout_Qt.setText(_("About &Qt"))
+        self.actionConfigure.setText(_("&Configure PySozluk-Qt"))
+        self.actionOffline.setText(_("&Offline"))
+        self.actionSave.setText(_("&Save"))
+        self.actionQuit.setText(_("&Quit"))
+        self.setWindowTitle(_("Pysozluk-Qt"))
+
 def main():
     app = QtGui.QApplication(sys.argv)
     app.setOrganizationName("pysozluk-qt")
@@ -64,7 +64,6 @@ def main():
     #import pycallgraph
     #pycallgraph.start_trace()
     core = pysozlukCore(ui)
-    retranslateUi(ui)
     ui.show() #dirty workaround for KDE startup notifier(should be shown later)
     makeConnections(ui, core, app)
     if settings.contains("windowPosition"):
