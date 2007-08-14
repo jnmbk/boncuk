da03ca465516ff577d6f2f04571f90b0e4f7a4bc
jnmbk <jnmbk@23da626b-2e77-438c-8063-04e1a2c34933>
2007-08-14 17:37:03 +0000

    change setup for new translation system

--- trunk/setup.py
+++ trunk/setup.py
@@ -16,49 +16,25 @@ import shutil
 from distutils.core import setup
 from distutils.command.build import build
 from distutils.command.clean import clean
-from distutils.command.install import install
-
-def getTranslations():
-    return ("tr",)
 
 def getVersion():
     import pysozlukqt.pysozlukglobals
     return pysozlukqt.pysozlukglobals.version
 
-def compileTranslations():
-    for i in getTranslations():
-        try:
-            os.mkdir("po/%s" %  i)
-        except OSError:
-            pass
-        os.system("msgfmt po/%s.po -o po/%s/pysozluk-qt.mo" % (i, i))
-
 class myBuild(build):
     def run(self):
         build.run(self)
-        compileTranslations()
+        #TODO: Don't hardcode this
+        os.system("lrelease-qt4 pysozlukqt/translations/pysozluk-qt_tr_TR.ts"
+            " -qm pysozlukqt/translations/pysozluk-qt_tr_TR.qm")
 
 class myClean(clean):
     def run(self):
         clean.run(self)
-        dirList = os.listdir("po")
-        for dir in dirList:
-            if os.path.isdir(os.path.join("po", dir)) and dir != ".svn":
-                shutil.rmtree(os.path.join("po", dir), ignore_errors = True)
-
-        dirList = ("build",)
-        for dir in dirList:
-            if os.path.exists(dir):
-                shutil.rmtree(dir)
-
-locales = []
-for i in getTranslations():
-    locales.append(("share/locale/%s/LC_MESSAGES" % i,
-            ["po/%s/pysozluk-qt.mo" % i]))
+        os.remove("pysozlukqt/translations/pysozluk-qt_tr_TR.qm")
 
 datas = [("share/applications",                ["pysozlukqt/data/pysozluk-qt.desktop"]),
          ("share/icons/hicolor/scalable/apps", ["pysozlukqt/data/pysozluk-qt.svg"])]
-datas.extend(locales)
 
 setup(name = "pysozluk-qt",
       version = getVersion(),
@@ -78,7 +54,8 @@ setup(name = "pysozluk-qt",
           "data/icons/16x16/*",
           "data/icons/32x32/*",
           "data/icons/64x64/*",
-          "data/icons/128x128/*",]},
+          "data/icons/128x128/*",
+          "translations/*.qm"]},
       data_files = datas,
       scripts = ["scripts/pysozluk-qt"],
       platforms = ["all"],
