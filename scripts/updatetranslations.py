23063bae29c676bd82f74338d0e2b56538ba8989
jnmbk <jnmbk@23da626b-2e77-438c-8063-04e1a2c34933>
2007-08-14 17:54:32 +0000

    configwindow came, not functional yet

---
+++ scripts/updatetranslations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+directory = "pysozlukqt"
+
+translateables = ("__init__.py",
+        "core.py",
+        "mainWindow.py",
+        "configWindow.py")
+
+command = "pylupdate4 "
+for file in translateables:
+    command += os.path.join(directory, file) + " "
+os.system(command + "-ts %s -verbose" % os.path.join(directory, "translations",
+    "pysozluk-qt_tr_TR.ts"))
