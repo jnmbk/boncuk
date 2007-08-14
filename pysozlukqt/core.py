354976dbd41cc65edde1b766676365ad5f39140b
jnmbk <jnmbk@23da626b-2e77-438c-8063-04e1a2c34933>
2007-08-14 17:03:35 +0000

    switch to Qt4 style translation

--- pysozlukqt/core.py
+++ pysozlukqt/core.py
@@ -14,11 +14,8 @@
 from PyQt4 import QtGui, QtCore
 from database import pysozlukDatabase
 from online import onlineDatabase
-from gettext import translation
 import pysozlukglobals
 import os
-
-_ = translation('pysozluk-qt', fallback=True).ugettext
 
 class searcherThread(QtCore.QThread):
     def __init__(self, keyword, settings):
@@ -39,8 +36,10 @@ class searcherThread(QtCore.QThread):
     def formatTranslation(self, translation):
         turkish = []
         english = []
-        tr = "<b>%s</b><br>" % _("Turkish translation of %s") % self.keyword
-        en = "<b>%s</b><br>" % _("English translation of %s") % self.keyword
+        tr = "<b>%s</b><br>" % QtGui.QApplication.translate("MainWindow",
+                "Turkish translation of %s") % self.keyword
+        en = "<b>%s</b><br>" % QtGui.QApplication.translate("MainWindow",
+                "English translation of %s") % self.keyword
         result = ""
 
         if translation:
@@ -62,7 +61,8 @@ class searcherThread(QtCore.QThread):
             self.sendResult(result)
         else:
             self.sendResult(
-                "<b>%s</b>" % _("Cannot find \"%s\"") % self.keyword)
+                "<b>%s</b>" % QtGui.QApplication.translate("MainWindow",
+                    "Cannot find \"%s\"") % self.keyword)
 
     def sendResult(self, result):
         self.emit(QtCore.SIGNAL("finishedSearching"), result)
@@ -84,7 +84,8 @@ class pysozlukCore:
         self.searcherThread = searcherThread(keyword, self.settings)
         self.ui.textBrowser.clearHistory()
         self.ui.textBrowser.setHtml(
-            "<b>%s</b>" % _("searching \"%s\"") % keyword)
+            "<b>%s</b>" % QtGui.QApplication.translate(
+                "searching \"%s\"") % keyword)
         QtCore.QObject.connect(self.searcherThread,
             QtCore.SIGNAL("finishedSearching"),
             self.ui.textBrowser.setHtml)
@@ -92,10 +93,10 @@ class pysozlukCore:
 
     def about(self):
         QtGui.QMessageBox.about(self.ui,
-            _("About Pysozluk-Qt"),
+            QtGui.QApplication.translate("AboutWindow", "About Pysozluk-Qt"),
             "Pysozluk-Qt v%s\n" % pysozlukglobals.version +\
             "http://pysozluk-qt.wiki.sourceforge.net\n\n" +\
-            _("Developers:") + "\n" +\
+            QtGui.QApplication.translate("AboutWindow", "Developers:") + "\n"+\
             u"İlker Kesen <ilker.kde at gmail.com>" + "\n" +\
             u"İşbaran Akçayır <isbaran at gmail.com>" + "\n" +\
             u"Uğur Çetin <ugur.jnmbk at gmail.com>")
@@ -110,13 +111,15 @@ class pysozlukCore:
     def save(self):
         if not self.ui.textBrowser.toPlainText():
             QtGui.QMessageBox.warning(self.ui,
-                _("Warning"),
-                _("If you want to save a description,"
-                " you must search a keyword."))
+                QtGui.QApplication.translate("MainWindow", "Warning"),
+                QtGui.QApplication.translate("MainWindow",
+                    "If you want to save a description,"
+                    " you must search a keyword."))
         else:
             #TODO: Add HTML and Plain Text file filters
             file_path = QtGui.QFileDialog.getSaveFileName(
-                self.ui, _("Save"), os.getenv("HOME"), "All Files (*)")
+                self.ui, QtGui.QApplication.translate("MainWindow", "Save"),
+                os.getenv("HOME"), "All Files (*)")
             if file_path:
                 if str(file_path).endswith(".html"):
                     description = self.ui.textBrowser.toHtml()
---
+++ pysozlukqt/data/translations/pysozluk-qt_tr_TR.ts
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="tr">
+<defaultcodec></defaultcodec>
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <location filename="core.py" line="96"/>
+        <source>About Pysozluk-Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.py" line="99"/>
+        <source>Developers:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="core.py" line="40"/>
+        <source>Turkish translation of %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.py" line="42"/>
+        <source>English translation of %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.py" line="65"/>
+        <source>Cannot find &quot;%s&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.py" line="114"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.py" line="117"/>
+        <source>If you want to save a description, you must search a keyword.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="core.py" line="121"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="100"/>
+        <source>Pysozluk-Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="101"/>
+        <source>Keyword:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="102"/>
+        <source>&amp;Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="106"/>
+        <source>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="107"/>
+        <source>&amp;File</source>
+        <translation>&amp;Dosya</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="108"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Yardım</translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="109"/>
+        <source>&amp;Index</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="110"/>
+        <source>F1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="111"/>
+        <source>About &amp;Pysozluk-Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="112"/>
+        <source>About &amp;Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="113"/>
+        <source>&amp;Offline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="114"/>
+        <source>Ctrl+M</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="115"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="116"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="117"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="118"/>
+        <source>Alt+F4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.py" line="119"/>
+        <source>&amp;Configure PySozluk-Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
