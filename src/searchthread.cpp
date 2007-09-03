8cd057ad1723b59a2afbb84ca5eb1606e927d559
jnmbk <jnmbk@23da626b-2e77-438c-8063-04e1a2c34933>
2007-09-03 09:45:20 +0000

    r184@pardus:  jnmbk | 2007-09-02 08:14:32 +0300  file command thought they were pascal source files :P

--- src/searchthread.cpp
+++ src/searchthread.cpp
@@ -1,14 +1,14 @@
-/************************************************************************
- * qt4sozluk, Qt port of pysozluk (online/offline dictionary)           *
- * Copyright (C) 2007, qt4sozluk Development Team                       *
- *                                                                      *
- * This program is free software; you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation; either version 2 of the License, or    *
- * (at your option) any later version.                                  *
- *                                                                      *
- * Please read the COPYING file.                                        *
- ************************************************************************/
+/*
+ * qt4sozluk, qt port of pysozluk (online/offline dictionary)
+ * copyright (c) 2007, qt4sozluk development team
+ *
+ * this program is free software; you can redistribute it and/or modify
+ * it under the terms of the gnu general public license as published by
+ * the free software foundation; either version 2 of the license, or
+ * (at your option) any later version.
+ *
+ * please read the copyIng file.
+ */
 
 #include <QDebug>
 #include <QSettings>
--- src/seslisozluk.cpp
+++ src/seslisozluk.cpp
@@ -1,14 +1,14 @@
-/************************************************************************
- * qt4sozluk, Qt port of pysozluk (online/offline dictionary)           *
- * Copyright (C) 2007, qt4sozluk Development Team                       *
- *                                                                      *
- * This program is free software; you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation; either version 2 of the License, or    *
- * (at your option) any later version.                                  *
- *                                                                      *
- * Please read the COPYING file.                                        *
- ************************************************************************/
+/*
+ * qt4sozluk, Qt port of pysozluk (online/offline dictionary)
+ * Copyright (C) 2007, qt4sozluk Development Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Please read the COPYING file.
+ */
 
 #include <QDebug>
 #include <QHttp>
