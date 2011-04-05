73d6c13c1074cc30439cbd752fbf0c42a8802c7e
jnmbk <jnmbk@23da626b-2e77-438c-8063-04e1a2c34933>
2011-04-05 03:43:11 +0000

    adapt to seslisozluk change

--- trunk/src/seslisozluk.cpp
+++ trunk/src/seslisozluk.cpp
@@ -79,11 +79,11 @@ void SesliSozluk::continueSearch(bool err)
     int i, j, turkish, english, german;
 
     text = codec->toUnicode(http->readAll());
-    // qDebug() << "HTML:" << text;
+    qDebug() << "HTML:" << text;
 
     //now we can parse it
     text.remove(0, text.indexOf('>', text.indexOf("tabsResult")) + 1);
-    text.remove(text.indexOf(" <!-- <div id=translations -->"), text.size());
+    text.remove(text.indexOf("<!-- id=translations -->"), text.size());
     if (text.count('<') != text.count('>')){
         emit found(word, results);
         return; //this will happen when we have a really bad syntax error
