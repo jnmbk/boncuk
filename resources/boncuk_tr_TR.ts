019573942e258f6aaee4b8cf599c4883f0df323b
jnmbk <jnmbk@23da626b-2e77-438c-8063-04e1a2c34933>
2011-09-08 10:37:55 +0000

    update translation

--- resources/boncuk_tr_TR.ts
+++ resources/boncuk_tr_TR.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="tr">
+<TS version="2.0" language="tr_TR">
 <context>
     <name>ConfigWindow</name>
     <message>
@@ -107,33 +107,81 @@
         <translation>Alt+F4</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="305"/>
+        <location filename="../src/mainwindow.cpp" line="297"/>
         <source>No results found</source>
         <translation>Sonuç bulunamadı</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="352"/>
+        <location filename="../src/mainwindow.cpp" line="343"/>
         <source>%1 translation of %2</source>
         <translation>%2 sözcüğünün %1 karşılığı</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="266"/>
-        <location filename="../src/mainwindow.cpp" line="267"/>
+        <location filename="../src/mainwindow.cpp" line="258"/>
+        <location filename="../src/mainwindow.cpp" line="259"/>
         <source>Searching &quot;%1&quot;</source>
         <translation>&quot;%1&quot; aranıyor</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="304"/>
+        <location filename="../src/mainwindow.cpp" line="122"/>
+        <source>History initialized for %1 words</source>
+        <translation>Geçmiş %1 sözcük için başlatıldı</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="296"/>
         <source>&lt;b&gt;No results found&lt;/b&gt;</source>
         <translation>&lt;b&gt;Sonuç bulunamadı&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="314"/>
+        <location filename="../src/mainwindow.cpp" line="306"/>
         <source>Found %1 results</source>
         <translation>%1 tane sonuç bulundu</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="382"/>
+        <location filename="../src/mainwindow.cpp" line="355"/>
+        <source>Boncuk %1 - online/offline dictionary
+This software is released under the terms of GPL v2.
+http://sourceforge.net/projects/pysozluk-qt/
+
+Developers:
+Ä°lker Kesen &lt;ilker.kde at gmail.com&gt;
+Ä°Åbaran AkÃ§ayÄ±r &lt;isbaran at gmail.com&gt;
+UÄur Ãetin &lt;ugur.jnmbk at gmail.com&gt;
+GÃ¶kmen GÃ¶rgen &lt;gkmngrgn at gmail.com&gt;
+</source>
+        <translation>Boncuk %1 - Ã§evrimiÃ§i/Ã§evrimdÄ±ÅÄ± sÃ¶zlÃ¼k
+Bu program GPL v2 lisansÄ± altÄ±nda daÄÄ±tÄ±lmaktadÄ±r.
+
+GeliÅtiriciler:
+Ä°lker Kesen &lt;ilker.kde at gmail.com&gt;
+Ä°Åbaran AkÃ§ayÄ±r &lt;isbaran at gmail.com&gt;
+UÄur Ãetin &lt;ugur.jnmbk at gmail.com&gt;
+GÃ¶kmen GÃ¶rgen &lt;gkmngrgn at gmail.com&gt;
+</translation>
+    </message>
+    <message>
+        <source>Boncuk %1 - online/offline dictionary
+This software is released under the terms of GPL v2.
+http://http://sourceforge.net/projects/pysozluk-qt/
+
+Developers:
+Ä°lker Kesen &lt;ilker.kde at gmail.com&gt;
+Ä°Åbaran AkÃ§ayÄ±r &lt;isbaran at gmail.com&gt;
+UÄur Ãetin &lt;ugur.jnmbk at gmail.com&gt;
+GÃ¶kmen GÃ¶rgen &lt;gkmngrgn at gmail.com&gt;
+</source>
+        <translation type="obsolete">Boncuk %1 - Çevrimiçi/Çevrimdışı sözlük
+Bu program GPL v2 lisansı altında dağıtılmaktadır.
+http://pysozluk-qt.wiki.sourceforge.net
+
+GeliÅtiriciler:
+Ä°lker Kesen &lt;ilker.kde at gmail.com&gt;
+Ä°Åbaran AkÃ§ayÄ±r &lt;isbaran at gmail.com&gt;
+UÄur Ãetin &lt;ugur.jnmbk at gmail.com&gt;
+</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="374"/>
         <source>Press Enter to begin search</source>
         <translation>Aramaya başlamak için Enter&apos;a basın</translation>
     </message>
@@ -155,7 +203,7 @@
     <message>
         <location filename="../ui/mainwindow.ui" line="182"/>
         <source>Show &amp;Search Button</source>
-        <translation>Arama Butonunu Göster</translation>
+        <translation>Arama &amp;Düğmesini Göster</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="195"/>
@@ -174,7 +222,7 @@
         <translation>Temizle</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="159"/>
+        <location filename="../src/mainwindow.cpp" line="151"/>
         <source>History deleted</source>
         <translation>Geçmiş silindi</translation>
     </message>
@@ -186,7 +234,7 @@
     <message>
         <location filename="../ui/mainwindow.ui" line="163"/>
         <source>&amp;Configure</source>
-        <translation>Yapılandır</translation>
+        <translation>&amp;Yapılandır</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="223"/>
@@ -194,7 +242,7 @@
         <translation>&amp;Temizle</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="372"/>
+        <location filename="../src/mainwindow.cpp" line="364"/>
         <source>About Boncuk</source>
         <translation>Boncuk Hakkında</translation>
     </message>
@@ -215,25 +263,24 @@
         <translation>Kapat</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="331"/>
-        <location filename="../src/mainwindow.cpp" line="336"/>
+        <location filename="../src/mainwindow.cpp" line="323"/>
+        <location filename="../src/mainwindow.cpp" line="328"/>
         <source>English</source>
         <translation>İngilizce</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="332"/>
-        <location filename="../src/mainwindow.cpp" line="337"/>
+        <location filename="../src/mainwindow.cpp" line="324"/>
+        <location filename="../src/mainwindow.cpp" line="329"/>
         <source>German</source>
         <translation>Almanca</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="333"/>
-        <location filename="../src/mainwindow.cpp" line="335"/>
+        <location filename="../src/mainwindow.cpp" line="325"/>
+        <location filename="../src/mainwindow.cpp" line="327"/>
         <source>Turkish</source>
         <translation>Türkçe</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="364"/>
         <source>Boncuk %1 - online/offline dictionary
 This software is released under the terms of GPL v2.
 http://pysozluk-qt.wiki.sourceforge.net
@@ -243,7 +290,7 @@ Developers:
 Ä°Åbaran AkÃ§ayÄ±r &lt;isbaran at gmail.com&gt;
 UÄur Ãetin &lt;ugur.jnmbk at gmail.com&gt;
 </source>
-        <translation>Boncuk %1 - Ã§evrimiÃ§i/Ã§evrimdÄ±ÅÄ± sÃ¶zlÃ¼k
+        <translation type="obsolete">Boncuk %1 - Ã§evrimiÃ§i/Ã§evrimdÄ±ÅÄ± sÃ¶zlÃ¼k
 Bu program GPL v2 lisansÄ± altÄ±nda daÄÄ±tÄ±lmaktadÄ±r.
 http://pysozluk-qt.wiki.sourceforge.net
 
@@ -279,7 +326,16 @@ p, li { white-space: pre-wrap; }
 &lt;td style=&quot;border: none;&quot;&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>what is that?</translatorcomment>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;table border=&quot;0&quot; style=&quot;-qt-table-type: root; margin-top:4px; margin-bottom:4px; margin-left:4px; margin-right:4px;&quot;&gt;
+&lt;tr&gt;
+&lt;td style=&quot;border: none;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
